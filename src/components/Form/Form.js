8ac73e88149e48627d9603e0e16dfe0580089962
Maksim <dolya.m.mos.av@gmail.com>
2023-10-10 00:20:33 +0300

    apdate code error

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -15,25 +15,25 @@ function Form({
   isLoading,
 }) {
   return (
-    <div class="form__container">
-      <Link to="/" class="form__logo">
+    <div className="form__container">
+      <Link to="/" className="form__logo">
         <img src={logo} alt="логотип" />
       </Link>
-      <h3 class="form__title">{title}</h3>
-      <form class="form" id="form" onSubmit={onSubmit} noValidate>
+      <h3 className="form__title">{title}</h3>
+      <form className="form" id="form" onSubmit={onSubmit} noValidate>
         {children}
         <button type="submit"
           disabled={isDisabled ? true : false}
-          class={isDisabled || isLoading
+          className={isDisabled || isLoading
             ? 'form__button-save form__button-save_inactive'
             : 'form__button-save'
           }>
           {buttonText}
         </button>
       </form>
-      <p class="form__text">
+      <p className="form__text">
         {question}
-        <Link to={link} class="form__link">
+        <Link to={link} className="form__link">
           {linkText}
         </Link>
       </p>

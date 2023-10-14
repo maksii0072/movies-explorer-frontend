acc6f09ed5cf9f80ee96ed75a33a668931315b1d
Maksim Dolya <dolya.m.mos.av@gmail.com>
2023-10-14 23:44:15 +0300

    refactoring code burger menu

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -1,39 +1,32 @@
-import { NavLink, Link } from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 import './BurgerMenu.css';
-import account from '../../images/icon_account.svg';
 
 function BurgerMenu({ menuOpen, closePopups }) {
   const navLinkClass = ({ isActive }) =>
     `menu__link ${isActive ? "menu__link_active" : ""}`;
-  const menuClassName = `menuElement ${menuOpen ? "menu_opened" : ""}`;
 
   return (
-    <div className={menuClassName}>
-      <div className='menu'>
-        <button className="menu__close-icon" type="button" aria-label="закрыть" onClick={closePopups}></button>
-        <ul className="menu__list">
-          <li className="menu__item">
-            <NavLink to="/" className='menu__link'>
+    <div class={`menu ${menuOpen ? "menu_opened" : ""}`}>
+      <div class="menu__container">
+        <button class="menu__close-icon" type="button" aria-label="закрыть" onClick={closePopups}></button>
+        <ul class="menu__list">
+          <li class="menu__item">
+            <NavLink to="/" class='menu__link'>
               Главная
             </NavLink>
           </li>
-          <li className="menu__item">
-            <NavLink to="/movies" className={navLinkClass}>
+          <li class="menu__item">
+            <NavLink to="/movies" class={navLinkClass}>
               Фильмы
             </NavLink>
           </li>
-          <li className="menu__item">
-            <NavLink to="/saved-movies" className={navLinkClass}>
+          <li class="menu__item">
+            <NavLink to="/saved-movies" class={navLinkClass}>
               Сохраненные фильмы
             </NavLink>
           </li>
         </ul>
-        <div className="menu__footer-box">
-          <Link className="menu__footer-box-account-button" to="/profile">Аккаунт</Link>
-          <Link to="/profile">
-            <img src={account} alt="Логотип аккаунта" className="menu__footer-box-account-image" />
-          </Link>
-        </div>
       </div>
     </div>
   );

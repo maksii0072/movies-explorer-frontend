e46ea72429f781b60c0815015e79b08cee857a42
Maksim Dolya <dolya.m.mos.av@gmail.com>
2023-10-20 02:21:15 +0300

    проверка работы лайков1

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -3,7 +3,7 @@ import './MoviesCard.css';
 import { durationConverter } from "../../utils/utils";
 
 function MoviesCard({ card, isSavedFilms, handleLikeClick, handleCardDelete, savedMovies, likedMovies }) {
-  const defaultSaved = savedMovies.some((m) => m.movieId === card.id);
+  const defaultSaved = savedMovies.findIndex((m) => m.movieId === card.id) !== -1;
   const [saved, setSaved] = useState(defaultSaved);
 
   function onCardClick() {
@@ -15,7 +15,7 @@ function MoviesCard({ card, isSavedFilms, handleLikeClick, handleCardDelete, sav
   }
 
   useEffect(() => {
-    if (likedMovies.some((m) => m.movieId === card.id)) {
+    if (likedMovies.findIndex((m) => m.movieId === card.id) !== -1) {
       setSaved(true);
     } else {
       setSaved(false);

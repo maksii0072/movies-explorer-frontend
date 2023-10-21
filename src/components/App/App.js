0ef3adc74d11337cf3592c3ca3f28a6c4851285d
Maksim Dolya <dolya.m.mos.av@gmail.com>
2023-10-21 13:33:58 +0300

    испралвние ошибки

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -174,22 +174,24 @@ function App() {
           console.log(err);
         });
   }
-
   function handleCardDelete(movie, setSaved) {
     const savedMovie = savedMovies.find(
       (card) => card.movieId === movie.id || card.movieId === movie.movieId
     );
-    api
-      .deleteSaveCard(savedMovie._id)
-      .then(() => {
-        setSavedMovies((state) =>
-          state.filter((item) => item._id !== savedMovie._id)
-        );
-        setSaved(false);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+
+    if (savedMovie) {
+      api
+        .deleteSaveCard(savedMovie._id)
+        .then(() => {
+          setSavedMovies((state) =>
+            state.filter((item) => item._id !== savedMovie._id)
+          );
+          setSaved(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }
 
   return (

366f98eafcb4826706bfbad696f0bbe593a8bb7a
Maksim Dolya <dolya.m.mos.av@gmail.com>
2023-10-22 23:06:55 +0300

    исправление карточек

--- src/utils/Constants/constants.js
+++ src/utils/Constants/constants.js
@@ -1,6 +1,6 @@
 export const DESKTOP_VERSION = 4;
 export const TABLET_VERSION = 3;
-export const MOBILE_VERSION = 2;
+export const MOBILE_VERSION = 1;
 export const DESKTOP = 1180;
 export const TABLET = 768;
 // eslint-disable-next-line no-useless-escape
@@ -8,5 +8,5 @@ export const EMAIL_REGEX = '^[^ ]+@[^ ]+\.[a-z]{2,3}$';
 export const USER_REGEX = '^[A-Za-zА-Яа-яЁё\\-\\s]+$';
 
 export const MOVIES_CARDS_1180 = 16;
-export const MOVIES_CARDS_768 = 12;
-export const MOVIES_CARDS_480 = 5;
+export const MOVIES_CARDS_768 = 8;
+export const MOVIES_CARDS_480 = 4;
